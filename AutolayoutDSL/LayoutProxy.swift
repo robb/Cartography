7c6c5eb8b120e294031d32b1329f38d4b4ea5396
Robert Böhnke <robb@robb.is>
2014-06-17 18:37:29 +0200

    Rename Layout to LayoutProxy

--- AutolayoutDSL/LayoutProxy.swift
+++ AutolayoutDSL/LayoutProxy.swift
@@ -1,5 +1,5 @@
 //
-//  Layout.swift
+//  LayoutProxy.swift
 //  AutolayoutDSL
 //
 //  Created by Robert Böhnke on 17/06/14.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class Layout {
+class LayoutProxy {
     let width: Dimension
     let height: Dimension
 
@@ -18,21 +18,21 @@ class Layout {
     }
 }
 
-func layout(view: UIView, block: Layout -> ()) {
-    block(Layout(view))
+func layout(view: UIView, block: LayoutProxy -> ()) {
+    block(LayoutProxy(view))
 
     view.layoutIfNeeded()
 }
 
-func layout(v1: UIView, v2: UIView, block: (Layout, Layout) -> ()) {
-    block(Layout(v1), Layout(v2))
+func layout(v1: UIView, v2: UIView, block: (LayoutProxy, LayoutProxy) -> ()) {
+    block(LayoutProxy(v1), LayoutProxy(v2))
 
     v1.layoutIfNeeded()
     v2.layoutIfNeeded()
 }
 
-func layout(v1: UIView, v2: UIView, v3: UIView, block: (Layout, Layout, Layout) -> ()) {
-    block(Layout(v1), Layout(v2), Layout(v3))
+func layout(v1: UIView, v2: UIView, v3: UIView, block: (LayoutProxy, LayoutProxy, LayoutProxy) -> ()) {
+    block(LayoutProxy(v1), LayoutProxy(v2), LayoutProxy(v3))
 
     v1.layoutIfNeeded()
     v2.layoutIfNeeded()
