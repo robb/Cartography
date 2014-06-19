ea7fa2e6b3bdf1b8851e1cca550a454bb15f0bab
Robert Böhnke <robb@robb.is>
2014-06-19 12:09:48 +0200

    Refactor operator tests

---
+++ CartographyTests/OperatorTests.swift
@@ -0,0 +1,64 @@
+//
+//  OperatorTests.swift
+//  Cartography
+//
+//  Created by Robert Böhnke on 18/06/14.
+//  Copyright (c) 2014 Robert Böhnke. All rights reserved.
+//
+
+import Cartography
+import UIKit
+import XCTest
+
+class OperatorTests: XCTestCase {
+    var superview: UIView!
+    var view: UIView!
+
+    override func setUp() {
+        superview = UIView(frame: CGRectMake(0, 0, 200, 200))
+
+        view = UIView(frame: CGRectZero)
+        superview.addSubview(view)
+    }
+
+    func testAddition() {
+        var constraint: NSLayoutConstraint!
+
+        layout(view, superview) { view, superview in
+            constraint = view.width == superview.width + 100
+        }
+
+        XCTAssertEqual(constraint.constant, 100, "It should set the constant")
+    }
+
+    func testSubtraction() {
+        var constraint: NSLayoutConstraint!
+
+        layout(view, superview) { view, superview in
+            constraint = view.width == superview.width - 100
+        }
+
+        XCTAssertEqual(constraint.constant, -100, "It should set the constant")
+    }
+
+    func testMultiplication() {
+        var constraint: NSLayoutConstraint!
+
+        layout(view, superview) { view, superview in
+            constraint = view.width == (superview.width + 100) * 2 + 5
+        }
+
+        XCTAssertEqual(constraint.multiplier, 2, "It should set the constant")
+        XCTAssertEqual(constraint.constant, 205, "It should multiply the constant")
+    }
+
+    func testPriority() {
+        var constraint: NSLayoutConstraint!
+
+        layout(view) { view in
+            constraint = (view.width == 200 ~ 100)
+        }
+
+        XCTAssertEqual(constraint.priority, 100, "It should set the priority")
+    }
+}
