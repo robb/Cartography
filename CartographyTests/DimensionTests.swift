45f12d53cf06637b3304338bf21008e316be6beb
Robert Böhnke <robb@robb.is>
2014-06-19 12:02:14 +0200

    Refactor dimension tests

--- CartographyTests/DimensionTests.swift
+++ CartographyTests/DimensionTests.swift
@@ -13,90 +13,43 @@ import XCTest
 class CartographyTests: XCTestCase {
     var superview: UIView!
     var view: UIView!
-    var view2: UIView!
 
     override func setUp() {
-        superview = UIView(frame: CGRectMake(0, 0, 100, 100))
+        superview = UIView(frame: CGRectMake(0, 0, 400, 400))
 
         view = UIView(frame: CGRectZero)
         superview.addSubview(view)
-
-        view2 = UIView(frame: CGRectZero)
-        superview.addSubview(view2)
     }
 
-    func testConstantEquality() {
-        layout(view) { view in
-            view.width == 200
-            200 == view.height
+    func testWidth() {
+        layout(view, superview) { view, superview in
+            view.width == superview.width - 200; return
         }
 
-        XCTAssertEqual(view.frame.size, CGSizeMake(200, 200), "should layout stuff")
+        XCTAssertEqual(CGRectGetWidth(view.frame), 200, "It should layout the width")
     }
 
-    func testAttributeEquality() {
-        layout(view) { view in
-            view.width == 200
-            view.width == view.height ~ 100
+    func testWidthConstants() {
+        layout(view, superview) { view, superview in
+            view.width == 200; return
         }
 
-        XCTAssertEqual(view.frame.size, CGSizeMake(200, 200), "should layout stuff")
+        XCTAssertEqual(CGRectGetWidth(view.frame), 200, "It should layout the width")
     }
 
-    func testInequalities() {
-        layout(view) { view in
-            200 <= view.width; view.width <= 300
-            view.height >= view.width
+    func testHeight() {
+        layout(view, superview) { view, superview in
+            view.height == superview.height - 200; return
         }
 
-        XCTAssertEqual(view.frame.size, CGSizeMake(200, 200), "should layout stuff")
+        XCTAssertEqual(CGRectGetHeight(view.frame), 200, "It should layout the height")
     }
 
-    func testAddition() {
-        layout(view) { view in
-            view.height == (50 + view.width + 100)
-            view.width == 200
+    func testHeightConstants() {
+        layout(view, superview) { view, superview in
+            view.height == 200; return
         }
 
-        XCTAssertEqual(view.frame.size, CGSizeMake(200, 350), "should layout stuff")
-    }
-
-    func testMultiplication() {
-        layout(view) { view in
-            view.height == 2 * view.width
-            view.width == 200
-        }
-
-        XCTAssertEqual(view.frame.size, CGSizeMake(200, 400), "should layout stuff")
-    }
-
-    func testPrecedence() {
-        layout(view) { view in
-            view.height == 2 * view.width + 50
-            view.width == 200
-        }
-
-        XCTAssertEqual(view.frame.size, CGSizeMake(200, 450), "should layout stuff")
-    }
-
-    func testParenthesis() {
-        layout(view) { view in
-            view.height == 2 * (view.width + 50) + 5
-            view.width == 200
-        }
-
-        XCTAssertEqual(view.frame.size, CGSizeMake(200, 505), "should layout stuff")
-    }
-
-    func testMultipleViews() {
-        layout(view, view2) { view, view2 in
-            2 * view2.height == view.height
-            view.width == ((view2.width + 200) + 100) - 100
-
-            view2.width  == 200
-            view2.height == 200
-        }
-
-        XCTAssertEqual(view.frame.size, CGSizeMake(400, 400), "should layout stuff")
+        XCTAssertEqual(CGRectGetHeight(view.frame), 200, "It should layout the height")
     }
 }
