13dabe28d13378b3066c8649fceb5004a1a7268b
Laura Savino <laura@khanacademy.org>
2014-10-21 17:05:35 -0400

    Differentiate between adding constraints and laying out subtree
    Summary: Callers may not want to cause a layout pass when adding constraints; `layout` maintains the previous behavior, but calling `constrain` opts out of calling layoutIfNeeded/layoutSubtreeIfNeeded.
    
    Test Plan: Add constraints to a view in `init` (before it's added to a window), and notice the lack of 'unsatisfiable constraints' warnings during the layout pass.
    
    Reviewers: mgp, andy
    
    Differential Revision: http://phabricator.khanacademy.org/D14045

--- Cartography/Layout.swift
+++ Cartography/Layout.swift
@@ -8,31 +8,50 @@
 
 import Foundation
 
-public func layout(view: View, block: LayoutProxy -> ()) {
+/// Adds constraints from `block` to the given view
+public func constrain(view: View, block: LayoutProxy -> ()) {
     block(LayoutProxy(view))
+}
 
-    view.car_updateAutoLayoutConstraints()
+/// Adds constraints from `block` to the given view & forces a layout pass before returning
+public func layout(view: View, block: LayoutProxy -> ()) {
+    constrain(view, block)
+
+    view.car_updateLayout()
+}
+
+public func constrain(v1: View, v2: View, block: (LayoutProxy, LayoutProxy) -> ()) {
+    block(LayoutProxy(v1), LayoutProxy(v2))
 }
 
 public func layout(v1: View, v2: View, block: (LayoutProxy, LayoutProxy) -> ()) {
-    block(LayoutProxy(v1), LayoutProxy(v2))
+    constrain(v1, v2, block)
 
-    v1.car_updateAutoLayoutConstraints()
-    v2.car_updateAutoLayoutConstraints()
+    v1.car_updateLayout()
+    v2.car_updateLayout()
+}
+
+public func constrain(v1: View, v2: View, v3: View, block: (LayoutProxy, LayoutProxy, LayoutProxy) -> ()) {
+    block(LayoutProxy(v1), LayoutProxy(v2), LayoutProxy(v3))
+
 }
 
 public func layout(v1: View, v2: View, v3: View, block: (LayoutProxy, LayoutProxy, LayoutProxy) -> ()) {
-    block(LayoutProxy(v1), LayoutProxy(v2), LayoutProxy(v3))
+    constrain(v1, v2, v3, block)
 
-    v1.car_updateAutoLayoutConstraints()
-    v2.car_updateAutoLayoutConstraints()
-    v3.car_updateAutoLayoutConstraints()
+    v1.car_updateLayout()
+    v2.car_updateLayout()
+    v3.car_updateLayout()
+}
+
+public func constrain(views: [View], block:([LayoutProxy]) -> ()) {
+    block(views.map({ LayoutProxy($0) }))
 }
 
 public func layout(views: [View], block:([LayoutProxy]) -> ()) {
-    block(views.map({ LayoutProxy($0) }))
+    constrain(views, block)
 
     for view in views {
-        view.car_updateAutoLayoutConstraints()
+        view.car_updateLayout()
     }
 }
