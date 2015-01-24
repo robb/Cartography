b2dcb7bf68160ddd2ed63449bd891216a5dd8b35
Robert Böhnke <robb@robb.is>
2015-01-24 16:46:25 +0100

    Remove layout and constrain from ConstraintGroup

--- Cartography/Layout.swift
+++ Cartography/Layout.swift
@@ -9,61 +9,83 @@
 import Foundation
 
 public func layout(view: View, replace group: ConstraintGroup = ConstraintGroup(), block: LayoutProxy -> ()) -> ConstraintGroup {
-    group.layout(view, block)
+    let context = Context()
+    block(LayoutProxy(context, view))
+    group.replaceConstraints(context.constraints, performLayout: true)
 
     return group
 }
 
 public func layout(v1: View, v2: View, replace group: ConstraintGroup = ConstraintGroup(), block: (LayoutProxy, LayoutProxy) -> ()) -> ConstraintGroup {
-    group.layout(v1, v2, block)
+    let context = Context()
+    block(LayoutProxy(context, v1), LayoutProxy(context, v2))
+    group.replaceConstraints(context.constraints, performLayout: true)
 
     return group
 }
 
 public func layout(v1: View, v2: View, v3: View, replace group: ConstraintGroup = ConstraintGroup(), block: (LayoutProxy, LayoutProxy, LayoutProxy) -> ()) -> ConstraintGroup {
-    group.layout(v1, v2, v3, block)
+    let context = Context()
+    block(LayoutProxy(context, v1), LayoutProxy(context, v2), LayoutProxy(context, v3))
+    group.replaceConstraints(context.constraints, performLayout: true)
 
     return group
 }
 
 public func layout(views: [View], replace group: ConstraintGroup = ConstraintGroup(), block: ([LayoutProxy]) -> ()) -> ConstraintGroup {
-    group.layout(views, block)
+    let context = Context()
+    block(views.map({ LayoutProxy(context, $0) }))
+    group.replaceConstraints(context.constraints, performLayout: true)
 
     return group
 }
 
 public func layout<T: Hashable>(views: [T: View], replace group: ConstraintGroup = ConstraintGroup(), block: ([T : LayoutProxy] -> ())) -> ConstraintGroup {
-    group.layout(views, block)
+    let context = Context()
+    let proxies = map(views) { ($0, LayoutProxy(context, $1)) }
+    block(Dictionary(proxies))
+    group.replaceConstraints(context.constraints, performLayout: true)
 
     return group
 }
 
 public func constrain(view: View, replace group: ConstraintGroup = ConstraintGroup(), block: LayoutProxy -> ()) -> ConstraintGroup {
-    group.constrain(view, block)
+    let context = Context()
+    block(LayoutProxy(context, view))
+    group.replaceConstraints(context.constraints, performLayout: false)
 
     return group
 }
 
 public func constrain(v1: View, v2: View, replace group: ConstraintGroup = ConstraintGroup(), block: (LayoutProxy, LayoutProxy) -> ()) -> ConstraintGroup {
-    group.constrain(v1, v2, block)
+    let context = Context()
+    block(LayoutProxy(context, v1), LayoutProxy(context, v2))
+    group.replaceConstraints(context.constraints, performLayout: false)
 
     return group
 }
 
 public func constrain(v1: View, v2: View, v3: View, replace group: ConstraintGroup = ConstraintGroup(), block: (LayoutProxy, LayoutProxy, LayoutProxy) -> ()) -> ConstraintGroup {
-    group.constrain(v1, v2, v3, block)
+    let context = Context()
+    block(LayoutProxy(context, v1), LayoutProxy(context, v2), LayoutProxy(context, v3))
+    group.replaceConstraints(context.constraints, performLayout: false)
 
     return group
 }
 
 public func constrain(views: [View], replace group: ConstraintGroup = ConstraintGroup(), block: ([LayoutProxy]) -> ()) -> ConstraintGroup {
-    group.constrain(views, block)
+    let context = Context()
+    block(views.map({ LayoutProxy(context, $0) }))
+    group.replaceConstraints(context.constraints, performLayout: false)
 
     return group
 }
 
 public func constrain<T: Hashable>(views: [T: View], replace group: ConstraintGroup = ConstraintGroup(), block: ([T : LayoutProxy] -> ())) -> ConstraintGroup {
-    group.constrain(views, block)
+    let context = Context()
+    let proxies = map(views) { ($0, LayoutProxy(context, $1)) }
+    block(Dictionary(proxies))
+    group.replaceConstraints(context.constraints, performLayout: false)
 
     return group
 }
