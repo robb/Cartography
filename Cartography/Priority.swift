8c18322e9f014a1d11e44ab26559c23da4e6b1de
Robert Böhnke <robb@robb.is>
2015-03-22 16:00:34 +0100

    Document ~

--- Cartography/Priority.swift
+++ Cartography/Priority.swift
@@ -14,12 +14,26 @@ import AppKit
 
 infix operator  ~ { }
 
+/// Sets the priority for a constraint.
+///
+/// :param: lhs The constraint to update.
+/// :param: rhs The new priority.
+///
+/// :returns: The same constraint with its priority updated.
+///
 public func ~ (lhs: NSLayoutConstraint, rhs: Float) -> NSLayoutConstraint {
     lhs.priority = rhs
 
     return lhs
 }
 
+/// Sets the priority for multiple constraints.
+///
+/// :param: lhs An array of `NSLayoutConstraint` instances.
+/// :param: rhs The new priority/
+///
+/// :returns: The same constraints with their priorities updated.
+///
 public func ~ (lhs: [NSLayoutConstraint], rhs: Float) -> [NSLayoutConstraint] {
     return lhs.map {
         $0 ~ rhs
