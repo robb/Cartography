876772b99f8dc0f9fc83dafdbcf633a959f35a20
Robert Böhnke <robb@robb.is>
2015-04-10 22:37:39 +0200

    Spec absense of memory leaks

---
+++ CartographyTests/MemoryLeakSpec.swift
@@ -0,0 +1,39 @@
+import Cartography
+
+import Nimble
+import Quick
+
+class MemoryLeakSpec: QuickSpec {
+    override func spec() {
+        describe("layout") {
+            it("should not leak memory") {
+                weak var weak_superview: View? = .None
+                weak var weak_viewA: View? = .None
+                weak var weak_viewB: View? = .None
+
+                autoreleasepool {
+                    let superview = View(frame: CGRectMake(0, 0, 400, 400))
+
+                    let viewA: View = View(frame: CGRectMake(0, 0, 200, 200))
+                    superview.addSubview(viewA)
+
+                    let viewB: View = View(frame: CGRectMake(0, 0, 200, 200))
+                    superview.addSubview(viewB)
+
+                    weak_superview = superview
+                    weak_viewA = viewA
+                    weak_viewB = viewB
+
+                    layout(viewA, viewB) { viewA, viewB in
+                        viewA.top    == viewB.top
+                        viewB.bottom == viewA.bottom
+                    }
+                }
+
+                expect(weak_superview).to(beNil())
+                expect(weak_viewA).to(beNil())
+                expect(weak_viewB).to(beNil())
+            }
+        }
+    }
+}
