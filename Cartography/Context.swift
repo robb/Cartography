f85c31a469e03d718b9793ccfc08eefd3ccaa398
Adam Nemecek <adamnemecek@gmail.com>
2017-03-16 14:35:14 -0700

    * refactoring

--- Cartography/Context.swift
+++ Cartography/Context.swift
@@ -50,25 +50,23 @@ public class Context {
                                                   attribute: to?.attribute ?? .notAnAttribute,
                                                   multiplier: CGFloat(coefficients.multiplier),
                                                   constant: CGFloat(coefficients.constant))
-
+        let view : View
         if let to = to {
-            guard let common = closestCommonAncestor(from.view, b: to.view) else {
-                fatalError("No common superview found between \(from.view) and \(to.view)")
-            }
-            constraints.append(Constraint(view: common, layoutConstraint: layoutConstraint))
+            guard let common = closestCommonAncestor(from.view, b: to.view) else { fatalError("No common superview found between \(from.view) and \(to.view)") }
+            view = common
         } else {
-            constraints.append(Constraint(view: from.view, layoutConstraint: layoutConstraint))
+            view = from.view
         }
 
+        constraints.append(Constraint(view: view, layoutConstraint: layoutConstraint))
         return layoutConstraint
     }
 
     internal func addConstraint(_ from: Compound, coefficients: [Coefficients]? = nil, to: Compound? = nil, relation: NSLayoutRelation = .equal) -> [NSLayoutConstraint] {
         return (0..<from.properties.count).map {
-            let n: Coefficients = coefficients?[$0] ?? Coefficients()
             return addConstraint(from.properties[$0],
                                  to: to?.properties[$0],
-                                 coefficients: n,
+                                 coefficients: coefficients?[$0] ?? Coefficients(),
                                  relation: relation)
         }
     }
