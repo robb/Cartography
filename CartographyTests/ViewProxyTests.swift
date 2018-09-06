fd4d42dc55702ec189fd9c5d376598d5925d035f
Luís Portela Afonso <me@portellaa.eu>
2018-09-06 15:50:09 +0100

    Simple helper for use safeAreaLayoutGuide if available

---
+++ CartographyTests/ViewProxyTests.swift
@@ -0,0 +1,40 @@
+//
+//  ViewProxyTests.swift
+//  Cartography-iOS-Tests
+//
+//  Created by Luís Portela on 01/10/2018.
+//  Copyright © 2018 Robert Böhnke. All rights reserved.
+//
+
+import Nimble
+import Quick
+import UIKit
+
+@testable import Cartography
+
+@available(iOS, introduced: 9.0)
+@available(tvOS, introduced: 9.0)
+final class ViewProxyTestsSpec: QuickSpec {
+    override func spec() {
+        describe("ViewProxy SafeArea Layout Guide") {
+
+            let view: UIView = UIView(frame: .zero)
+
+            constrain(view) { testingView in
+                if #available(iOS 11, *), #available(tvOS 11, *) {
+                    context("When running on iOS 11+ devices") {
+                        it("safeArea should be SafeAreaLayoutGuide") {
+                            expect(testingView.safeArea.item).to(beIdenticalTo(testingView.safeAreaLayoutGuide.item))
+                        }
+                    }
+                } else {
+                    context("When runnig prior versions of iOS 11") {
+                        it("safeArea should be LayoutMarginsGuide") {
+                            expect(testingView.safeArea.item).to(beIdenticalTo(testingView.layoutMarginsGuide.item))
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
